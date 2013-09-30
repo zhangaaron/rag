a673d80c26456387f23a1e0faa3cc880dcc92e8c
Armando Fox <fox@cs.berkeley.edu>
2013-09-29 18:22:47 -0700

    basic queue methods specd and working

--- lib/x_queue.rb
+++ lib/x_queue.rb
@@ -1,4 +1,5 @@
 class XQueue
+  require 'debugger'
   require 'mechanize'
   require 'json'
   
@@ -6,34 +7,54 @@ class XQueue
   # (autograders).  Lets you pull student-submitted work products from a
   # named queue and post back the results of grading them.
   #
-  # == Authentication
+  # All responses from the XQueue server have a JSON object with
+  # +return_code+ and +content+ slots.  A +return_code+ of 0 normally
+  # means success.  
+  #
+  # == Example
   #
   # You need two sets of credentials to authenticate yourself to the
   # xqueue server.  For historical reasons, they are called
   # (+django_name+, +django_pass+) and (+user_name+, +user_pass+).
   # You also need to name the queue you want to use; edX creates queues
   # for you.  Each +XQueue+ instance is tied to a single queue name.
+  #
+  # === Retrieving an assignment:
+  #
+  #     queue = XQueue.new('dj_name', 'dj_pass', 'u_name', 'u_pass', 'my_q')
+  #     queue.length  # => an integer showing queue length
+  #     assignment = XQueue::Submission.new(queue.get_submission) # => the 'content' slot of the JSON response body
+  #     assignment.raw_data # => contents of file the student uploaded
+  #
+  # === Posting results back
+  #
+  # The submission includes a secret key that is used in postback,
+  # so the postback method is defined on the submission not the queue object.
+  #
+  #     assignment.msg = "Feedback to student"
+  #     assignment.score = 90  # => points out of total possible
+  #     assignment.correct = true  # => show green checkmark vs red "x"
+  #     assignment.post_grade_response!
+  #
 
+  # The base URI of the production Xqueue server.
+  XQUEUE_DEFAULT_BASE_URI = 'https://xqueue.edx.org'
 
-  attr_reader :queue_name
+  # Error message, if any, associated with last unsuccessful operation
+  attr_reader :error
+  
   # Queue from which to pull, established in constructor.  You need a
   # new +XQueue+ object if you want to use a different queue.
+  attr_reader :queue_name
 
-  attr_reader :base_uri
   # The base URI used for this queue; won't change for this queue even
   # if you later change the value of +XQueue.base_uri+
+  attr_reader :base_uri
 
-  attr_reader :session_cookie
-  # The session cookie resulting from a successful authentication to
-  # queue server 
-
-  XQUEUE_DEFAULT_BASE_URI = 'https://xqueue.edx.org'
-  # The base URI of the production server where queues live.
-
+  # The base URI used when new queue instances are created
   def self.base_uri
     @@base_uri ||= URI(XQUEUE_DEFAULT_BASE_URI)
   end
-
   def self.base_uri=(uri)
     @@base_uri = URI(uri)
   end
@@ -42,9 +63,11 @@ class XQueue
   # Raised if XQueue authentication fails
   class IOError < StandardError ; end
   # Raised if there are network or I/O errors connecting to queue server
+  class NoSuchQueueError < StandardError ; end
+  # Raised if queue name doesn't exist
 
-  # Creates a new queue instance and attempts to authenticate to the
-  # queue server.
+  # Creates a new instance and attempts to authenticate to the
+  # queue server.  
   # * +django_name+, +django_pass+: first set of auth credentials (see
   # above)
   # * +user_name+, +user_pass+: second set of auth credentials (see
@@ -56,55 +79,87 @@ class XQueue
     @django_auth = {'username' => django_name, 'password' => django_pass}
     @session = Mechanize.new
     @session.add_auth(@base_uri, user_name, user_pass)
+    @valid_queues = nil
+    @error = nil
+    @authenticated = nil
   end
 
+  # Authenticates to the server.  You can call this explicitly, but it
+  # is called automatically if necessary on the first request in a new
+  # session.  
   def authenticate
-    response = @session.post_request(:post, 'xqueue/login/', @django_auth)
-    if (error_message = login_error(response)).nil?
-      @session_cookie = response['set-cookie'].split(';').first
+    response = request :post, '/xqueue/login/', @django_auth
+    if response['return_code'] == 0
+      @authenticated = true
     else
-      raise AuthenticationError, error_message
+      raise(AuthenticationError, "Authentication failure: #{response['content']}")
     end
   end
 
+  # Returns +true+ if the session has been properly authenticated to
+  # server, that is, after a successful call to +authenticate+ or to any
+  # of the request methods that may have called +authenticate+ automatically.
+  def authenticated? ; @authenticated ; end
+
+  # Returns length of the queue as an integer >= 0.
   def queue_length
-    response = send_request(:get, 'xqueue/get_queuelen/', :queue_name =>
-      @queue_name)
-    
+    authenticate unless authenticated?
+    response = request(:get, '/xqueue/get_queuelen/', {:queue_name => @queue_name})
+    if response['return_code'] == 0 # success
+      response['content'].to_i
+    elsif response['return_code'] == 1 && response['content'] =~ /^Valid queue names are: (.*)/i
+      @valid_queues = $1.split(/,\s+/)
+      raise NoSuchQueueError, "No such queue: valid queues are #{$1}"
+    else
+      raise IOError, response['content']
+    end
+  end
+
+  # Returns a list of all valid queue names; something of a hack since
+  # an +XQueue+ instance is supposed to be tied to a queue name...
+  def list_queues
+    authenticate unless authenticated?
+    if @valid_queues.nil?
+      old, @queue_name = @queue_name, 'I_AM_NOT_A_QUEUE'
+      begin queue_length rescue nil end
+    end
+    @valid_queues
+  end
+
+  # Record a result of grading something.  It may be easier to use
+  # +XQueue::Submission#post_back+, which marshals the information
+  # needed here automatically.
+  #
+  # * +header+: secret header key (from 'xqueue_header' slot in the
+  # 'content' object of the original retrieved submission)
+  # * +score+: integer number of points (not scaled)
+  # * +correct+: true (default) means show green checkmark, else red 'x'
+  # * +message+: (optional) plain text feedback; will be coerced to UTF-8
+
+  def put_result(header, score, correct=true, message='')
+    payload = JSON.generate({
+        :xqueue_header => header,
+        :xqueue_body => {
+          :correct   => (!!correct).to_s.capitalize,
+          :score     => score,
+          :message   => message.encode('UTF-8',
+            :invalid => :replace, :undef => :replace, :replace => '?'),
+        }
+      })
+    request :post, '/xqueue/put_result', payload
   end
 
   private
 
-  def login_error(response)
-    return "Server error: #{response.code} #{response.message}" unless
-      response.kind_of? Net::HTTPSuccess
+  def request(method, path, args={})
     begin
+      response = @session.send(method, @base_uri + path, args)
       response_json = JSON(response.body)
-      return (response_json['return_code'].zero? ?
-        nil :
-        "Authentication failure: #{response_json['content']}")
     rescue JSON::ParserError => e
-      return "Non-JSON response from server: #{response.body}"
+      raise IOError, "Non-JSON response from server: #{response.body.force_encoding('UTF-8')}"
+    rescue Exception => e
+      raise IOError, e.message
     end
-  end
-
-  def send_request(method, path, form_data=nil)
-    begin
-      uri = URI.join(base_uri, path)
-      request = (method == :post ?
-        Net::HTTP::Post.new(uri.request_uri) :
-        Net::HTTP::Get.new(uri.request_uri))
-      request.basic_auth(*@basic_auth)
-      request.set_form_data(form_data) if form_data
-      request['cookie'] = session_cookie if session_cookie
-      Net::HTTP.start(uri.host, uri.port,
-        :use_ssl => (uri.scheme == 'https'),
-        :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |connection|
-        connection.request(request)
-      end
-    end
-  rescue Exception => e
-    raise IOError, e.message
   end
 
 end
--- spec/x_queue_spec.rb
+++ spec/x_queue_spec.rb
@@ -2,16 +2,27 @@
 # Setting XXX and YYY to either valid or invalid django_auth credentials,
 #   and setting USER:PASS to the valid or invalid BasicAuth username/password:
 # For testing authentication:
-# curl --include --silent --user 'USER:PASS' \
-#      --cookie-jar /tmp/cookies.txt'  \
-#      --data 'username=XXX' --data 'password=YYY' \
-#      https://xqueue.edx.org/xqueue/login/
-# For testing queue length:
+# curl --include --silent --user 'USER:PASS' --cookie-jar /tmp/cookies.txt'  \
+#      --data-urlencode 'username=XXX' --data-urlencode 'password=YYY' \
+#      https://sandbox-xqueue.edx.org/xqueue/login/
+# For testing queue length: establish a valid session as above, then
+# curl --include --silent  --cookie /tmp/cookies.txt  \
+#      --data-urlencode 'queue_name=QNAME'  \
+#      https://sandbox-xqueue.edx.org/xqueue/get_queuelen
 
 require 'x_queue'
+require 'fakeweb'
 
 describe XQueue do
 
+  before(:each) { FakeWeb.allow_net_connect = false }
+  after(:each) { FakeWeb.clean_registry }
+  
+  def fixture_response(method, file)
+    FakeWeb.register_uri(method.to_sym, %r|^https://.*xqueue.edx.org/|,
+      :response => "spec/fixtures/#{file}.txt")
+  end
+  
   describe 'base URI' do
     after(:all) { XQueue.base_uri = XQueue::XQUEUE_DEFAULT_BASE_URI }
     it 'has a default' do
@@ -30,6 +41,44 @@ describe XQueue do
     subject { XQueue.new('django_user', 'django_pass', 'user', 'pass', 'my_q') }
     its(:queue_name) { should == 'my_q' }
     its(:base_uri)   { should == URI('https://xqueue.edx.org') }
+    its(:error)      { should be_nil }
+    it { should_not be_authenticated }
+  end
+
+  describe 'authenticating' do
+    it 'with invalid credentials should raise XQueue::AuthenticationError' do
+      fixture_response(:post, 'x_queue_bad_credentials')
+      expect { XQueue.new('bad', 'bad', 'bad', 'bad', 'q1').authenticate }.
+        to raise_error(XQueue::AuthenticationError,
+        'Authentication failure: Incorrect login credentials')
+    end
+    it 'with valid credentials should not raise error' do
+      fixture_response(:post, 'x_queue_successful_auth')
+      queue = XQueue.new('good','good','good','good','q1')
+      expect { queue.authenticate }.not_to raise_error
+      queue.should be_authenticated
+    end
+  end
+
+  describe 'valid session' do
+    before :each do
+      @q = XQueue.new('good','good','good','good','my_queue')
+      @q.stub(:authenticated?).and_return(true)
+    end
+    it 'should return list of queue names' do
+      fixture_response(:get, 'x_queue_no_such_queue')
+      @q.list_queues.should == %w(test-pull test-2)
+    end
+    describe 'get queue length' do
+      it 'for existing queue should return an integer' do
+        fixture_response(:get, 'x_queue_queuelength')
+        @q.queue_length.should == 101
+      end
+      it 'for nonexistent queue should raise NoSuchQueueError' do
+        fixture_response(:get, 'x_queue_no_such_queue')
+        expect { @q.queue_length }.to raise_error(XQueue::NoSuchQueueError)
+      end
+    end
   end
 
 end
